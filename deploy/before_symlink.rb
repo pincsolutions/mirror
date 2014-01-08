bb92c8315c120e5e259b1172454ab07831a3e628
nroose <nroose@gmail.com>
2014-01-08 11:40:31 -0800

    adding an instance variable to the deploy hook erb eval to test.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,6 +1,6 @@
 template "#{release_path}/config/config.yml" do
   local true
-  test_var = "test me!"
+  @test_var = "test me!"
   cas = node[:cas]
   source "#{release_path}/config/config.yml.erb"
 end
