915ed9d1c8e7a6927b10f34c400fbe5d7c5f8553
nroose <nroose@gmail.com>
2014-01-08 12:15:05 -0800

    Using old hash syntax.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,5 @@
 template "#{release_path}/config/config.yml" do
   local true
-  variables({test_var: "Test Me!", cas: node[:cas]})
+  variables({:test_var => "Test Me!", :cas => node[:cas]})
   source "#{release_path}/config/config.yml.erb"
 end
